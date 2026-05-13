4ae8e5d1aadcaa4f62e2b9c17acc0fb630252c4d
KristinaBrandstetter <s2510892004@fhooe.at>
2026-05-13 18:07:42 +0200

    Connect4

--- js/model.connectfour.js
+++ js/model.connectfour.js
@@ -10,6 +10,212 @@
 //      First step: Create your model-object with all the properties
 //      necessary to store that information.
 
+
+export let gameModel = {
+    PLAYER1: {
+        name: "Tom",
+        id: 1,
+        color: "grey",
+    },
+
+    PLAYER2: {
+        name: "Jerry",
+        id: 2,
+        color: "brown",
+    },
+
+    gamingField: [
+        [0, 0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0, 0,],
+        [0, 0, 0, 0, 0, 0, 0,],
+    ],
+
+    currentPlayer: null,
+    init() {
+        this.currentPlayer = this.PLAYER1;
+    },
+
+    gameCounter: 2,
+    gameOver: false,
+    winningStones: [],
+
+    insertStone(col) {
+        if (this.gamingField [0][col] === 1 || this.gamingField [0][col] === 2){
+            this.dispatchColumnFull(col)
+            return;
+        } else {
+            for (let i = 5; i >= 0; i--) {
+                if (this.gamingField[i][col] === 0) {
+                    this.gamingField[i][col] = this.currentPlayer.id;
+                    this.dispatchInsertStone();
+                    this.checkWin(i, col);
+                    this.checkDraw()
+                    if (!this.gameOver) {
+                        this.setCurrentPlayer();
+                    }
+                    break;
+
+                }
+            }
+        }
+
+    },
+
+    setCurrentPlayer() {
+        if (this.gameCounter % 2 == 0) {
+            this.currentPlayer = this.PLAYER2;
+            this.gameCounter++;
+        } else {
+            this.currentPlayer = this.PLAYER1
+            this.gameCounter++;
+        }
+        this.dispatchPlayerChange()
+    },
+
+    checkDraw() {
+        for (let i = 0; i < 7; i++) {
+            if (this.gamingField[0][i] === 0) {
+                return;
+            }
+        }
+        this.gameOver = true;
+        this.dispatchGameOver(null);
+    },
+
+    checkWin(row, col) {
+        let counter = 0;
+        for (let i = 0; i <= 6; i++) { //checks horizintal
+            if (this.gamingField[row][i] === this.currentPlayer.id) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+            if (counter === 4) {
+                let winner = this.currentPlayer;
+                this.gameOver = true;
+                this.winningStones[0] = { row: row, col: i };
+                this.winningStones[1] = { row: row, col: i - 1 };
+                this.winningStones[2] = { row: row, col: i - 2 };
+                this.winningStones[3] = { row: row, col: i - 3 };
+                this.dispatchGameOver(winner);
+                return
+            }
+        }
+        counter = 0;
+        for (let i = 0; i <= 5; i++) { //checks vertical
+            if (this.gamingField[i][col] === this.currentPlayer.id) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+            if (counter === 4) {
+                let winner = this.currentPlayer;
+                this.gameOver = true;
+                this.winningStones[0] = { row: i, col: col };
+                this.winningStones[1] = { row: i - 1, col: col };
+                this.winningStones[2] = { row: i - 2, col: col};
+                this.winningStones[3] = { row: i - 3, col: col };
+                this.dispatchGameOver(winner);
+                return
+            }
+        }
+
+        let tempStones1 = [];
+        tempStones1.push({ row: row, col: col });
+
+        let counterDiag1 = 0;
+        for (let i = 1; row + i <= 5 && col + i <= 6; i++) {
+            if (this.gamingField[row + i][col + i] === this.currentPlayer.id) {
+                counterDiag1++;
+                tempStones1.push({ row: row + i, col: col + i})
+
+            } else {
+                break;
+            }
+        }
+        for (let i = 1; row - i >= 0 && col - i >= 0; i++) {
+            if (this.gamingField[row - i][col - i] === this.currentPlayer.id) {
+                counterDiag1++;
+                tempStones1.push({ row: row - i, col: col - i})
+            } else {
+                break;
+            }
+        }
+        if (counterDiag1 + 1 >= 4) {
+            let winner = this.currentPlayer;
+            this.gameOver = true;
+            this.winningStones = tempStones1;
+            this.dispatchGameOver(winner);
+            return;
+        }
+
+        let tempStones2 = [];
+        tempStones2.push({ row: row, col: col });
+
+        let counterDiag2 = 0;
+        for (let i = 1; row - i >= 0 && col + i <= 6; i++) {
+            if (this.gamingField[row  -i][col + i] === this.currentPlayer.id) {
+                counterDiag2++;
+                tempStones2.push({ row: row - i, col: col + i})
+
+            } else {
+                break;
+            }
+        }
+        for (let i = 1; row + i <= 5 && col - i >= 0; i++) {
+            if (this.gamingField[row + i][col - i] === this.currentPlayer.id) {
+                counterDiag2++;
+                tempStones2.push({ row: row + i, col: col - i})
+            } else {
+                break;
+            }
+        }
+        if (counterDiag2 + 1 >= 4) {
+            let winner = this.currentPlayer;
+            this.gameOver = true;
+            this.winningStones = tempStones2;
+            this.dispatchGameOver(winner);
+            return;
+        }
+
+    },
+
+    dispatchPlayerChange() {
+        const playerChange = new CustomEvent("connectfour:playerchange", {
+            detail: {currentPlayer: this.currentPlayer}
+        });
+        document.dispatchEvent(playerChange);
+    },
+
+    dispatchInsertStone() {
+        const insertStone = new CustomEvent("connectfour:insertStone", {
+            detail: {gamingField: this.gamingField}
+        });
+        document.dispatchEvent(insertStone);
+    },
+
+    dispatchGameOver(winner) {
+        const gameOver = new CustomEvent("connectfour:gameOver", {
+            detail: {
+                winner: winner,
+                winningStones: this.winningStones,
+            }
+        });
+        document.dispatchEvent(gameOver);
+    },
+
+    dispatchColumnFull(col) {
+        const columnFull = new CustomEvent("connectfour:columnFull", {
+            detail: {col: col}
+        });
+        document.dispatchEvent(columnFull);
+    }
+
+}
+
 //TODO: Prepare some customEvents. The model should dispatch events when
 //      - The Player Changes
 //      - A stone was inserted
@@ -18,6 +224,7 @@
 //      For each customEvent, just make a >method< for your model-object,
 //      that, when called, dispatches the event. Nothing else should
 //      happen in those methods.
+
 
 
 //TODO: Initiate the battlefield. Your model needs a representation of the
@@ -31,11 +238,10 @@
 //      Hint: This method will be called later by your controller, when the
 //      user makes an according input.
 
+
 //TODO: Methods to check if the game is over, either by draw or a win.
 //      Let the world know in both cases what happend. If it's a win,
 //      Don't forget to store the winning stones and add this >detail<
 //      to your custom event.
 
 //TODO: Method to change the current player (and dispatch the according event).
-
-bababa
